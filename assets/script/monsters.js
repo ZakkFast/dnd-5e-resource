46d6ab925582c86d68776df9a9477d2220466035
ZakkFast <Zakkfastpro@gmail.com>
2021-07-31 21:54:09 -0700

    finished up to legendary actions

--- assets/script/monsters.js
+++ assets/script/monsters.js
@@ -22,18 +22,41 @@ $(document).ready(function(){
                         success: function(data){
                             console.log(data)
                             $('#contentContainer').html(`
-                            <div class='container'>
+                            <div class='container' id='containerEl'>
                                 <h3>${data.name}</h3>
                                 <p>Size: ${data.size}</p>
-                                <p>Challenge: ${data.challenge_rating}</p>
-                                <ul>Attributes:</u>
-                                <li>Str ${data.strength}</li> 
-                                <li>Dex ${data.dexterity}</li> 
-                                <li>Con ${data.constitution}</li> 
-                                <li>Int ${data.intelligence}</li>
-                                <li>Wis ${data.wisdom}</li> 
-                                <li>Cha ${data.charisma}</li>
+                                <p>Challenge Rating: ${data.challenge_rating}</p>
+                                <p>Experience: ${data.xp}</p>
+                                <p>Armor Class: ${data.armor_class}</p>
+                                <p>Hit Points: ${data.hit_points}</p>
+                                <p>Hit Dice: ${data.hit_dice}</p>
+                                <ul>Attributes:
+                                <li class='descli'>Str ${data.strength}</li> 
+                                <li class='descli'>Dex ${data.dexterity}</li> 
+                                <li class='descli'>Con ${data.constitution}</li> 
+                                <li class='descli'>Int ${data.intelligence}</li>
+                                <li class='descli'>Wis ${data.wisdom}</li> 
+                                <li class='descli'>Cha ${data.charisma}</li>
+                                </ul>
+                                <ul id='actionEl'>
+                                Actions:
+                                </ul>
+                                <ul id='legendaryActionsEl'>
+                                </ul>
                             `)
+                            for(i = 0; i < data.actions.length; i++){
+                                let actionData = $(`<li class='descli'>${data.actions[i].name}: ${data.actions[i].desc}</li>`)
+
+                                $('#actionEl').append(actionData)
+                            }
+                            if(data.legendary_actions.length > 0){
+                                $(`#legendaryActionsEl`).text('Legendary Actions:')
+                            }
+                            for(i = 0; i < data.legendary_actions.length; i++){
+                                let legendaryAction = $(`<li class='descli'>${data.legendary_actions[i].name}: ${data.legendary_actions[i].desc}</li>`)
+
+                                $('#legendaryActionsEl').append(legendaryAction)
+                            }
                         }
                     })
                 })
