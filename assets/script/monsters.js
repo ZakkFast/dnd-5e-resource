17aa2dc1c5748ba9a00db80616d3875577087c86
ZakkFast <Zakkfastpro@gmail.com>
2021-07-31 20:50:30 -0700

    api call, user input, some pop done

--- assets/script/monsters.js
+++ assets/script/monsters.js
@@ -6,7 +6,37 @@ $(document).ready(function(){
             url: `${apiurl}monsters`,
             dataType: 'json',
             success: function(data){
-                console.log(data)
+                let selectEl = $(`<select></select>`)
+                for (i = 0; i < data.results.length; i++){
+                    let divContainer = $(`#divDrop`)
+                    let optionEl = $(`<option value='${data.results[i].index}'>${data.results[i].name}</option>`)
+                    divContainer.append(selectEl)
+                    selectEl.append(optionEl)
+                }
+                $('#divDrop').change(function(){
+                    let selectedVal = $(this).find(':selected').val()
+                    $.ajax({
+                        type: 'GET',
+                        url: `${apiurl}monsters/${selectedVal}`,
+                        dataType: 'json',
+                        success: function(data){
+                            console.log(data)
+                            $('#contentContainer').html(`
+                            <div class='container'>
+                                <h3>${data.name}</h3>
+                                <p>Size: ${data.size}</p>
+                                <p>Challenge: ${data.challenge_rating}</p>
+                                <ul>Attributes:</u>
+                                <li>Str ${data.strength}</li> 
+                                <li>Dex ${data.dexterity}</li> 
+                                <li>Con ${data.constitution}</li> 
+                                <li>Int ${data.intelligence}</li>
+                                <li>Wis ${data.wisdom}</li> 
+                                <li>Cha ${data.charisma}</li>
+                            `)
+                        }
+                    })
+                })
             }
         })
     } 
