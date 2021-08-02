5922d8635b68ff4f1a24eb02cd8378107db4e058
ZakkFast <Zakkfastpro@gmail.com>
2021-08-01 19:13:56 -0700

    finished race appending

--- assets/script/races.js
+++ assets/script/races.js
@@ -24,13 +24,56 @@ $(document).ready(function(){
                             $('#contentContainer').html(`
                             <div class='container'>
                                 <h3>${data.name}</h3>
-                                <p>Size: ${data.size}</p>
+                                <p>Size: ${data.size_description}</p>
                                 <p>Age: ${data.age}</p>
                                 <p>Speed: ${data.speed}
                                 <p>Languages: ${data.language_desc}
                                 <p>Alignment: ${data.alignment}</p>
+                                <ul id='traits'>
+                                Traits:
+                                </ul>
+                                <ul id='startProf'>
+                                Starting Proficiencies:
+                                </ul>
+                                <ul id='abilityBns'>
+                                Ability Bounses:
+                                </ul>
+                                <ul id='subraces'>
+                                Subraces:
+                                </ul>
                             </div>
                             `)
+                            if(data.traits.length > 0) {
+                            data.traits.forEach(traits => {
+                                let traitsLi = $(`<li class='descli'>${traits.name}</li>`)
+                                $('#traits').append(traitsLi)
+                            });
+                            } else {
+                                let traitsLi = $(`<li class='descli'>None</li>`)
+                                $('#traits').append(traitsLi)
+                            }
+                            if(data.starting_proficiencies.length > 0){
+                                data.starting_proficiencies.forEach(startProf => {
+                                    let startProfEl = $(`<li class='descli'>${startProf.name}</li>`)
+                                    $('#startProf').append(startProfEl)
+                                })
+                            } else {
+                                let startProfEl = $(`<li class='descli'>None</li>`)
+                                $('#startProf').append(startProfEl)
+                            }
+                            if(data.subraces.length > 0){
+                                data.subraces.forEach(subraces => {
+                                    let subraceEl = $(`<li class='descli'>${subraces.name}</li>`)
+                                    $('#subraces').append(subraceEl)
+                                })
+                            } else {
+                                let subraceEl = $(`<li class='descli'>None</li>`)
+                                $('#subraces').append(subraceEl)
+                            }
+                            data.ability_bonuses.forEach(ability_bonuses => {
+                                let abilityBnsEl = $(`<li class='descli'>${ability_bonuses.ability_score.name}: ${ability_bonuses.bonus}</li>`)
+                                $('#abilityBns').append(abilityBnsEl)
+                            })
                         }
                     })
                 })
